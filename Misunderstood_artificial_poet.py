7a77606c071c35a592c9b7290e8002c035bd0c51
RandomJungle <38086198+RandomJungle@users.noreply.github.com>
2018-07-09 16:50:00 +0200

    solved issue with rhyming system, only problem is now only a small set of words are used

--- Misunderstood_artificial_poet.py
+++ Misunderstood_artificial_poet.py
@@ -8,7 +8,7 @@ from nltk.text import Text
 from nltk import ConditionalFreqDist, FreqDist
 from nltk.collocations import *
 
-class Misunderstood_genius:
+class Misunderstood_artificial_poet:
 	
 	def __init__(self, master) :
 		'''	Constructor. master is a string that names a directory in the same repository that contains all the work from inspiration
@@ -18,7 +18,7 @@ class Misunderstood_genius:
 		self.text = self.reader.words()
 		
 	def generate_model(self, word, num=50):
-		'''	write a text based on most probable word to appear after each word. Prone to looping
+		'''	Writes a text based on most probable word to appear after each word. Prone to looping
 		'''
 		bigrams = nltk.bigrams(self.text)
 		cfdist = nltk.ConditionalFreqDist(bigrams)
@@ -28,7 +28,7 @@ class Misunderstood_genius:
 			word = cfdist[word].max()
 			
 	def count_foot(self, word):
-		''' counts number of foot in word (doesn't account for jonctions for now)
+		''' Counts number of foot in word (doesn't account for jonctions for now)
 		'''	
 		startsWithVowel = False
 		if re.match('[aeiouyàéèùôûâêîïöüäëÿ]', word):
@@ -40,7 +40,7 @@ class Misunderstood_genius:
 		return(len(cleansplit))
 		
 	def check_rhyme(self, language, string, substring):
-		''' checks if the end of a string rhymes with a substring (could this be implemented via machine learning ?)
+		''' Checks if the end of a string rhymes with a substring (could this be implemented via machine learning ?)
 		'''
 		pattern = re.sub('(.*?)([aeiouyàéèùôûâêîïöüäëÿ][zrtpmlkjhgfdsqwxcvbn]*?$)', '\2', string)
 		if language == 'french':
@@ -49,7 +49,7 @@ class Misunderstood_genius:
 			pass
 
 	def text_generator(self, word, num=10):
-		''' write a text based on a random choice of word that appear in collocation in master's work
+		''' Writes a text based on a random choice of word that appear in collocation in master's work
 		'''
 		verse = ""
 		bigrams = nltk.bigrams(self.text)
@@ -62,8 +62,8 @@ class Misunderstood_genius:
 			word = random.choice(word_collocates)
 		return verse
 	
-	def rhyme_generator(self, word, rhyme=None, foot=12):
-		''' write a verse based on previous word used
+	def rhyme_generator(self, inputWord, rhyme=None, foot=12):
+		''' Writes a verse based on previous word used
 		'''	
 		verse = ""
 		counted_foot = 0
@@ -74,23 +74,24 @@ class Misunderstood_genius:
 			rhyme = random.choice([word for word in self.text if (len(word) > 3 and re.match('[a-zA-Z]', word))])[-3:]
 		while continueWriting:
 			word_collocates = []
-			for w in cfdist[word] :
+			for w in cfdist[inputWord] :
 				word_collocates.append(w)
 			if counted_foot < foot-2 :
 				word = random.choice(word_collocates)
 				verse += word + ' '
 			else :
-				rhyming_collocates = [word for word in word_collocates if word.endswith(rhyme) and self.count_foot(word) == 2]
+				rhyming_collocates = [word for word in word_collocates if (word.endswith(rhyme) and self.count_foot(word) == 2 and word != inputWord)]
 				if not rhyming_collocates:
-					rhyming_collocates = [word for word in self.text if word.endswith(rhyme) and self.count_foot(word) == 2]
+					rhyming_collocates = [word for word in self.text if (word.endswith(rhyme) and self.count_foot(word) == 2 and word != inputWord)]
 				word = random.choice(rhyming_collocates)
 				verse += word + ' '
 				continueWriting= False
 			counted_foot += self.count_foot(word)
+		verse = re.sub(' $', '', verse)
 		return verse
 		
 	def compose_standard_poem(self, length):
-		'''	write a poem with each verse starting with most commons words in master's work
+		'''	Writes a poem with each verse starting with most commons words in master's work
 		'''	
 		poem =''
 		all_word_dist = nltk.FreqDist(w.lower() for w in self.text)
@@ -101,7 +102,7 @@ class Misunderstood_genius:
 		return poem
 			
 	def compose_random_poem(self, length):
-		'''	write a poem with each verse starting with random words from master's work
+		'''	Writes a poem with each verse starting with random words from master's work
 		'''
 		poem = ''
 		for word in random.sample([x for x in self.text if re.search('[a-zA-Z]', x) is not None], length):
@@ -129,18 +130,21 @@ class Misunderstood_genius:
 		'''	
 		final_work = []
 		first_word = random.choice([w.lower() for w in self.text if re.search('[a-zA-Z]', w) is not None])
-		verse = self.rhyme_generator(word=first_word, foot=12, rhyme=None)
+		verse = self.rhyme_generator(inputWord=first_word, foot=12, rhyme=None)
+		print(verse)
 		final_work.append(verse)
-		first_word = verse.split(' ')[-2]
-		previous_rhyme = verse[-3]
+		first_word = verse.split(' ')[-1]
+		previous_rhyme = verse[-3:]
 		for i in range(2,length):
+			print(previous_rhyme)
 			if i%2 != 0:
-				verse = self.rhyme_generator(word=first_word, rhyme=None)
-				previous_rhyme = verse[-3]
+				verse = self.rhyme_generator(inputWord=first_word, rhyme=None)
+				previous_rhyme = verse[-3:]
 				final_work.append(verse)
 			else:
-				verse = self.rhyme_generator(word=first_word, rhyme=previous_rhyme)
+				verse = self.rhyme_generator(inputWord=first_word, rhyme=previous_rhyme)
 				final_work.append(verse)
+			print(verse)
 		final_work = "\n".join(final_work)
 		return final_work
 			
@@ -164,6 +168,6 @@ class Misunderstood_genius:
 				
 if __name__ == "__main__" : 
 	
-	spleener = Misunderstood_genius('hugo')
+	spleener = Misunderstood_artificial_poet('hugo')
 	work_title = spleener.find_title()
 	spleener.draft_manuscript(title=work_title, func=spleener.compose_rhyming_poem, length=10, foot=12)
